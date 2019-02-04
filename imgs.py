bf80b45cd837d80dd84cf37453723a28ddab6c2d
tjacek <tjacek@student.agh.edu.pl>
2019-02-04 19:55:34 +0100

    torch dataset

--- imgs.py
+++ imgs.py
@@ -1,6 +1,7 @@
 import seq.io
 import numpy as np
 from scipy.interpolate import CubicSpline
+import torch,torch.utils.data
 
 class SplineUpsampling(object):
     def __init__(self,new_size=128):
@@ -17,14 +18,18 @@ class SplineUpsampling(object):
     	print(new_x.shape)
     	return cs(new_x)
 
-def img_action(in_path,out_path):
+def img_transform(in_path,out_path=None):
     upsampling=SplineUpsampling()
     def action_helper(img_seq):
         img_seq=np.array(img_seq).T
         img_action=np.array([ upsampling(feat_i) for feat_i in img_seq])
-        print(img_action.shape)
         return [img_action]
-    seq.io.transform_actions(in_path,out_path,action_helper,
-    	img_in=False,img_out=True,whole_seq=True)
+    
+    new_actions=seq.io.transform_actions(in_path,out_path,action_helper,
+    	            img_in=False,img_out=True,whole_seq=True)
+    new_actions=torch.stack([torch.Tensor(action_i.img_seq[0]) for action_i in new_actions])
+    return torch.utils.data.TensorDataset(new_actions)
+    #new_actions=np.array([action_i.img_seq[0] for action_i in new_actions])
+    #print(new_actions.shape)
 
-img_action('all','imgs')
+img_transform('all')#,'imgs')
