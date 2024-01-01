dd6f4328f9653fb065f89274a1ae5c0673ec2331
tjacek <tjacek@agh.edu.pl>
2024-01-01 08:06:07 +0100

    linear separ

---
+++ separ.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gen_points(n,C=3):
+    return C*(np.random.random(size=(n,2))-0.5)
+
+def get_y(points,r=1):
+    r2=r*r
+    y=[int(np.sum(point_i*point_i)<r2)
+           for point_i in points]
+    return np.array(y)
+
+def polar_cord(X):
+    polar=[]
+    for x_i,y_i in X:
+        r_i=np.sqrt(x_i**2+y_i**2)
+#        φ=np.arccos(x_i/r_i)
+        φ=np.arctan2(x_i,y_i)
+        polar.append([r_i,φ])
+#        print(x_i-r_i*np.cos(φ))
+    return np.array(polar)
+	          
+
+def show(X,y,xlabel='x',ylabel='y'):
+    plt.clf()
+    X_zero=X[y==0]
+    plt.scatter(X_zero[:,0], X_zero[:,1])
+    X_one=X[y==1]
+    plt.scatter(X_one[:,0], X_one[:,1])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+X=gen_points(200)
+y=get_y(X)
+show(X,y)
+
+X_polar=polar_cord(X)
+#print(X_polar)
+show(X_polar,y,xlabel='r',ylabel='φ')
