e9edee5d78ec52b2b1a5f61c3b2353dfd8eca0a7
tjacek <tjacek@agh.edu.pl>
2024-06-20 21:02:53 +0200

    confusion matrix

---
+++ cf.py
@@ -0,0 +1,22 @@
+import sklearn
+import seaborn as sn
+import matplotlib.pyplot as plt
+import compare,train
+
+def gen_cf(in_path,alg_type='DTW-FEATS'):
+    all_pairs=train.read_pairs(in_path)
+    alg=compare.get_alg(alg_type)
+    for type_i,(y_pred,y_test) in alg(all_pairs):
+        if(type_i=='all'):
+            print(type_i)
+            cf=sklearn.metrics.confusion_matrix(y_test, y_pred)
+            print(cf)
+            sn.heatmap(cf,
+                       cmap="YlGnBu",
+                       annot=True,
+                       annot_kws={"size": 5}, 
+                       fmt='g')
+            plt.show()
+
+in_path=f'../DTW'
+gen_cf(f'{in_path}/MSR',alg_type='Cechy DTW')    
