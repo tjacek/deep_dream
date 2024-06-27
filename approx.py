ee1109ee4381bfc6e845e94dd77a8621a3efcdb5
tjacek <jacekt89@gmail.com>
2024-06-27 20:29:51 +0200

    approx

--- approx.py
+++ approx.py
@@ -1,13 +1,16 @@
 import json,time
 from sklearn.metrics import classification_report
 from dtaidistance import dtw_ndim
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+
 import dtw,seq,train,utils
 
 def dtw_exp(in_path:str,
 	        out_path:str,
 	        distance_type="base"):
     seq_dict=seq.read_seq(in_path)
-    seq_dict= seq_dict.subset(30)
+#    seq_dict= seq_dict.subset(30)
     distance_fun=get_distance_fun(distance_type)
     start = time.time()
     dtw_pairs=dtw.make_pairwise_distance(seq_dict,
@@ -23,8 +26,12 @@ def dtw_exp(in_path:str,
     return dtw_pairs
 
 def get_distance_fun(distance_type):
-    if(distance_type=="base"):
-        return dtw_ndim.distance
+    if(distance_type=="fast"):
+        def dist(seq_i,seq_j):
+            distance, path = fastdtw(seq_i, seq_j, dist=euclidean)
+            return distance
+        return dist
+    return dtw_ndim.distance
 
 def eval_pairs(in_path):
     pairs=dtw.read_pairs(in_path)
@@ -34,7 +41,7 @@ def eval_pairs(in_path):
 
 def multiexp(in_path,out_path):
     feats_type=['max_z','corl','skew','std']
-    dtw_types=['base']
+    dtw_types=['fast','base']
     utils.make_dir(out_path)
     for feat_i in feats_type:
         in_i=f'{in_path}/{feat_i}/seqs.npz'
@@ -44,7 +51,7 @@ def multiexp(in_path,out_path):
 	                out_path=out_ij,
 	                distance_type=dtw_j)
 
-multiexp("data/MSR","data/MSR")
+#multiexp("data/MSR","data/MSR")
 #in_path="data/MSR/corl/seqs.npz"
 #dtw_exp(in_path,'test')
-#eval_pairs('test/pairs')
+eval_pairs('data/MSR/max_z/fast/pairs')
